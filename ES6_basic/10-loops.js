0695e2e69e5b5fe39c9b11271c0b3518367eb426
vnporras <vnporras1028@gmail.com>
2024-07-21 13:14:57 -0500

    add task

---
+++ ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+export default function appendToEachArrayValue(array, appendString) {
+    for (const [index, value] of array.entries()) {
+      array[index] = appendString + value;
+    }
+  
+    return array;
+  }
+  
---
+++ ES6_basic/8-getBudgetCurrentYear.js
@@ -0,0 +1,15 @@
+function getCurrentYear() {
+    const date = new Date();
+    return date.getFullYear();
+  }
+  
+  export default function getBudgetForCurrentYear(income, gdp, capita) {
+    const year = getCurrentYear();
+  
+    return {
+      [`income-${year}`]: income,
+      [`gdp-${year}`]: gdp,
+      [`capita-${year}`]: capita,
+    };
+  }
+  
