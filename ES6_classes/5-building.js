a21b7aa814a1095f2c7df7c96168181ce7745346
vnporras <vnporras1028@gmail.com>
2024-07-29 01:29:27 -0500

    change implementation

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,17 +1,17 @@
 class Building {
     constructor(sqft) {
-        this._sqft = sqft;
+      this._sqft = sqft;
     }
 
     get sqft() {
-        return this._sqft;
+      return this._sqft;
     }
 
     static validateSubclass(subclass) {
-        if (subclass.prototype.evacuateWarningMessage === undefined) {
-            throw new Error('Class extending Building must override evacuationWarningMessage');
-        }
+      if (subclass.prototype.evacuateWarningMessage === undefined) {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+      }
     }
-}
+  }
 
-export default Building;
+  export default Building;
