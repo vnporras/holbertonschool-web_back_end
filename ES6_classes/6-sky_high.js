d1efd4df70fca0a75c4f04384ea292df7129c530
vnporras <vnporras1028@gmail.com>
2024-07-29 01:42:08 -0500

    change

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -1,22 +1,22 @@
 import Building from './5-building';
 
 class SkyHighBuilding extends Building {
-    constructor(sqft, floors) {
-        super(sqft);
-        this._floors = floors;
-    }
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
 
-    get sqft() {
-        return this._sqft;
-    }
+  get sqft() {
+    return this._sqft;
+  }
 
-    get floors() {
-        return this._floors;
-    }
+  get floors() {
+    return this._floors;
+  }
 
-    evacuationWarningMessage() {
-        return `Evacuate slowly the ${this._floors} floors`;
-    }
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
 }
 
 export default SkyHighBuilding;
