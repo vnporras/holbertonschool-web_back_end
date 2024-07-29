c2ba939ce0681533c8798948e9233c3b2a36482e
vnporras <vnporras1028@gmail.com>
2024-07-29 01:57:38 -0500

    change

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,15 +1,15 @@
 export default class Car {
-    constructor(brand, motor, color) {
-      this._brand = brand;
-      this._motor = motor;
-      this._color = color;
-    }
-  
-    cloneCar() {
-      const cloned = new this.constructor();
-      cloned._brand = undefined;
-      cloned._motor = undefined;
-      cloned._color = undefined;
-      return cloned;
-    }
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
   }
+  
+  cloneCar() {
+    const cloned = new this.constructor();
+    cloned._brand = undefined;
+    cloned._motor = undefined;
+    cloned._color = undefined;
+    return cloned;
+  }
+}
