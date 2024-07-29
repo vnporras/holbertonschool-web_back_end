73eea4db3f7cda1e30a84ea9d1fdc17da6367b38
vnporras <vnporras1028@gmail.com>
2024-07-29 02:48:24 -0500

    identacion

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,6 @@
 export default function cleanSet(set, startString) {
   return Array.from(set)
-    .filter(value => value.startsWith(startString))
-    .map(value => value.slice(startString.length))
+    .filter((value) => value.startsWith(startString))
+    .map((value) => value.slice(startString.length))
     .join('-');
 }
