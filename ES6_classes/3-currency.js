ce54018ad242ce80ae725f49eb943e229c7914aa
vnporras <vnporras1028@gmail.com>
2024-07-29 01:21:29 -0500

    change indentation

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,35 +1,35 @@
 class Currency {
-    constructor(code, name) {
-      this._code = this.verifyStr(code, 'Code');
-      this._name = this.verifyStr(name, 'Name');
-    }
+  constructor(code, name) {
+    this._code = this.verifyStr(code, 'Code');
+    this._name = this.verifyStr(name, 'Name');
+  }
 
-    verifyStr(value, attribute) {
-      if (typeof value !== 'string') {
-        throw new TypeError(`${attribute} must be a string`);
-      }
-      return value;
+  verifyStr(value, attribute) {
+    if (typeof value !== 'string') {
+      throw new TypeError(`${attribute} must be a string`);
     }
-  
-    get code() {
-      return this._code;
-    }
-  
-    set code(value) {
-      this._code = this.verifyStr(value, 'Code');
-    }
-  
-    get name() {
-      return this._name;
-    }
-  
-    set name(value) {
-      this._name = this.verifyStr(value, 'Name');
-    }
-  
-    displayFullCurrency() {
-      return `${this._name} (${this._code})`;
-    }
+    return value;
   }
   
-  export default Currency;
+  get code() {
+    return this._code;
+  }
+  
+  set code(value) {
+    this._code = this.verifyStr(value, 'Code');
+  }
+  
+  get name() {
+    return this._name;
+  }
+  
+  set name(value) {
+    this._name = this.verifyStr(value, 'Name');
+  }
+  
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
+  
+export default Currency;
