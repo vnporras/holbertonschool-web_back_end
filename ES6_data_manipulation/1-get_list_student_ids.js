f84c720766a1e8d7263f98b3ff932bb5a790f965
vnporras <vnporras1028@gmail.com>
2024-07-29 02:37:20 -0500

    identacion

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,4 +1,4 @@
 export default function getListStudentIds(students) {
   if (!Array.isArray(students)) return [];
-    return students.map((student) => student.id);
+  return students.map((student) => student.id);
 }
