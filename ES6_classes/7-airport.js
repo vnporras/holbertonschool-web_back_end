caddb9de5b56f2d95af1d0f363075e6200937c14
vnporras <vnporras1028@gmail.com>
2024-07-29 01:43:38 -0500

    change

--- ES6_classes/7-airport.js
+++ ES6_classes/7-airport.js
@@ -1,14 +1,14 @@
 export default class Airport {
-    constructor(name, code) {
-      this._name = name;
-      this._code = code;
-    }
-  
-    toString() {
-      return `[object ${this._code}]`;
-    }
-  
-    [Symbol.for('nodejs.util.inspect.custom')]() {
-      return `Airport [${this._code}] { _name: '${this._name}', _code: '${this._code}' }`;
-    }
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
   }
+
+  toString() {
+    return `[object ${this._code}]`;
+  }
+
+  [Symbol.for('nodejs.util.inspect.custom')]() {
+    return `Airport [${this._code}] { _name: '${this._name}', _code: '${this._code}' }`;
+  }
+}
