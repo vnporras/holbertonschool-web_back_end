b31201ca88c7b422219ddd0007f455073a9c7cf5
vnporras <vnporras1028@gmail.com>
2024-08-23 11:28:04 -0500

    modified pycodestyle

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -8,6 +8,7 @@ import asyncio
 import random
 from typing import Generator
 
+
 async def async_generator() -> Generator[float, None, None]:
     """
     Asynchronously generates ten random floating-point numbers
