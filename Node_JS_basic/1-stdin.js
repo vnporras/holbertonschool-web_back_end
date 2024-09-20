ba38ff4c3178bd3170bd2903b948c84f80950dc9
vnporras <vnporras1028@gmail.com>
2024-09-20 15:34:55 -0500

    Changes

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,8 +1,10 @@
+const os = require('os');
+
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
-  process.stdout.write(`Your name is: ${name}\r`);
-  process.stdout.write('This important software is now closing\n');
+  process.stdout.write(`Your name is: ${name}${os.EOL}`);
+  process.stdout.write(`This important software is now closing${os.EOL}`);
   process.stdin.end();
 });
